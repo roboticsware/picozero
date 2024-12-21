78c8579c4e36370789e30cfa42c31b9182d713da
JJ Lee <comseong@gmail.com>
2024-12-21 12:31:07 +0500

    Fix a readocs build fail issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 __project__ = 'picozero-rw'
 __packages__ = ['picozero']
-__desc__ = 'A beginner-friendly library for using common electronics components with the Raspberry Pi Pico.'
+__desc__ = 'This is a fork of the origin picozero library'
 __version__ = '0.4.3'
 __author__ = "Roboticsware"
 __author_email__ = 'roboticsware_uz@gmail.com'
