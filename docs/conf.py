9a8ccad2646caf4c5d9054606b742470a81c0e3a
JJ Lee <comseong@gmail.com>
2026-04-27 12:14:12 +0500

    docs: configure readthedocs build settings and sphinx documentation environment

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+# -- Path setup --------------------------------------------------------------
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-# Mock out certain modules while building documentation
+# [추가] RTD 대시보드에서 설정할 환경 변수를 읽어옵니다 (기본값 'en')
+# 이 변수는 나중에 RTD 프로젝트 설정에서 'ko', 'uz' 등으로 넣을 겁니다.
+MY_DOC_LANG = os.environ.get('MY_DOC_LANG', 'en')
+
+# Mock out certain modules
 class Mock:
     __all__ = []
     def __init__(self, *args, **kw): pass
@@ -35,52 +28,44 @@ class Mock:
 sys.modules['machine'] = Mock()
 sys.modules['micropython'] = Mock()
 
-# add the ticks_ms function to time (as it is in micropython)
 import time
 setattr(time, 'ticks_ms', lambda x: None)
 setattr(time, 'ticks_us', lambda x: None)
 
 # -- Project information -----------------------------------------------------
-
 project = 'picozero'
 copyright = '2022, Raspberry Pi Foundation'
 author = 'Raspberry Pi Foundation'
-
-# The full version, including alpha/beta/rc tags
 release = '0.4.5'
 
-
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.autodoc', 
     'sphinx.ext.viewcode', 
     'sphinx.ext.intersphinx'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# [수정] 언어별로 빌드할 때 다른 언어 폴더가 포함되지 않도록 제외 설정을 동적으로 변경합니다.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+if MY_DOC_LANG == 'ko':
+    # 한국어 빌드 시: 영어 루트 파일들과 타 언어 폴더 제외
+    exclude_patterns.extend(['uz/**', 'index.rst']) 
+    language = 'ko'
+elif MY_DOC_LANG == 'uz':
+    # 우즈벡어 빌드 시: 영어 루트 파일들과 타 언어 폴더 제외
+    exclude_patterns.extend(['ko/**', 'index.rst'])
+    language = 'uz'
+else:
+    # 영어(기본) 빌드 시: 하위 언어 폴더들 제외
+    exclude_patterns.extend(['ko/**', 'uz/**'])
+    language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 if on_rtd:
     html_theme = 'sphinx_rtd_theme'
-    #html_theme_options = {}
     html_sidebars = {
         '**': [
             'globaltoc.html',
@@ -90,15 +75,9 @@ if on_rtd:
     }
 else:
     html_theme = 'alabaster'
-    #html_theme_options = {}
-    #html_sidebars = {}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # -- Autodoc configuration ------------------------------------------------
-
 autodoc_member_order = 'groupwise'
 autodoc_default_flags = ['members']
